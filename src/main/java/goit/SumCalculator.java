795286cd134ff1ef34664cd097871065bc7bba61
AcceleratorMX <redfield242@gmail.com>
2023-03-25 23:25:03 +0200

    1st

--- src/main/java/goit/SumCalculator.java
+++ src/main/java/goit/SumCalculator.java
@@ -2,7 +2,7 @@ package goit;
 
 public class SumCalculator {
 
-        public static int sum ( int n){
+        public static int sum (int n){
             return switch (n) {
                 case 0 -> throw new IllegalArgumentException();
                 default -> (n * (n + 1)) / 2;
